66fa528f70061b28eb8721d82638eace935b5255
zzangin <zzangin1919@gmail.com>
2024-11-10 10:58:33 +0900

    초기 커밋.

---
+++ TcpManager/Model/RecvMessage.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TcpManager.Model
+{
+	public class RecvMessage
+	{
+	}
+}
