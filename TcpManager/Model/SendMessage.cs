615f855c7f795f5c25c22b2408283060322726dd
zzangin <zzangin1919@gmail.com>
2024-11-10 12:19:36 +0900

    SendMessage 포맷 작업

--- TcpManager/Model/SendMessage.cs
+++ TcpManager/Model/SendMessage.cs
@@ -8,5 +8,21 @@ namespace TcpManager.Model
 {
 	public class SendMessage
 	{
+		#region => Field
+
+		public string Cmd;
+		public int ReturnValue;
+
+		#endregion => Field
+
+		#region => Constructor
+
+		public SendMessage(string _cmd, int _returnValue = 0)
+		{
+			Cmd = _cmd;
+			ReturnValue = _returnValue;
+		}
+
+		#endregion => Constructor
 	}
 }
