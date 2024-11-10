cbbfbed6cf6059c761ef399a4f8b5aa31b8aaf85
zzangin <zzangin1919@gmail.com>
2024-11-10 22:49:13 +0900

    클라이언트 개수에 따른 생성자 분리

--- TcpManager/Model/SendMessage.cs
+++ TcpManager/Model/SendMessage.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -12,15 +13,29 @@ namespace TcpManager.Model
 
 		public string Cmd;
 		public int ReturnValue;
+		public TcpClient Client;
 
 		#endregion => Field
 
 		#region => Constructor
 
+		/// <summary>
+		/// 단일 클라이언트일 때 Server에서 Client로 보내기 전 Queue 데이터
+		/// </summary>
 		public SendMessage(string _cmd, int _returnValue = 0)
 		{
 			Cmd = _cmd;
 			ReturnValue = _returnValue;
+		}
+
+		/// <summary>
+		/// 멀티 클라이언트일 때 Server에서 Client로 보내기 전 Queue 데이터
+		/// </summary>
+		public SendMessage(TcpClient _client, string _cmd, int _returnValue = 0)
+		{
+			Cmd = _cmd;
+			ReturnValue = _returnValue;
+			Client = _client;
 		}
 
 		#endregion => Constructor
